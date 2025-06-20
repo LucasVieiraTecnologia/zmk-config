0fefe9b4b88dfa81b881261269f081dae3f40cb4
Lucas Augusto <97644357+LucasVieiraTecnologia@users.noreply.github.com>
2025-06-20 13:26:29 -0300

    Update display_manager.c

--- config/display_manager.c
+++ config/display_manager.c
@@ -5,21 +5,30 @@
 #include <lvgl.h>
 #include <zmk/display/widgets/widget.h>
 #include <zmk/event_manager.h>
-#include <zmk/display.h> // ZMK display core functions
+#include <zmk/display.h>
+#include <zephyr/logging/log.h> // <--- ADICIONE ESTA LINHA para habilitar o log
 
 // Inclua o cabeçalho do seu widget de animação
-#include "sofle_animation_widget.h"
+#include "sofle_oled_animation.h"
 
-LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+// Registre um módulo de log para o seu código customizado
+// (Altere 'zmk' para 'zmk_custom_display' para evitar conflitos se zmk já estiver declarado)
+LOG_MODULE_REGISTER(zmk_custom_display, CONFIG_ZMK_LOG_LEVEL); // <--- ALTERE/ADICIONE ESTA LINHA
 
 static const struct device *display_dev;
 static lv_obj_t *zmk_display_screen;
 
-// Esta função será chamada pelo ZMK durante a inicialização do firmware.
 int zmk_display_custom_init() {
-    LOG_DBG("Inicializando display ZMK personalizado para Sofle");
+    // <--- ADICIONE ESTA LINHA NO INÍCIO DA FUNÇÃO
+    LOG_INF("!!!!!!! ZMK Custom Display Init Called !!!!!!!");
 
     display_dev = DEVICE_DT_GET(DT_CHOSEN(zmk_display));
+
+    // Adicione uma verificação de ponteiro nulo para o dispositivo
+    if (!display_dev) {
+        LOG_ERR("ZMK Display Device not found!");
+        return -ENODEV;
+    }
 
     if (!device_is_ready(display_dev)) {
         LOG_ERR("Display device not ready");
@@ -37,9 +46,9 @@ int zmk_display_custom_init() {
     }
     lv_scr_load(zmk_display_screen);
 
-    // Inicializa e adiciona seu widget de animação à tela.
     zmk_widget_animation_init(NULL);
 
-    LOG_DBG("Display ZMK personalizado inicializado com animação.");
+    // <--- ADICIONE ESTA LINHA NO FIM DA FUNÇÃO
+    LOG_INF("!!!!!!! ZMK Custom Display Init Finished !!!!!!!");
     return 0;
 }
